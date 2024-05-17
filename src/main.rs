2ffa1faad810761809483514725b6f11d8afaa30
Florian Bernard <florian@flob.fr>
2024-05-18 00:13:54 +0200

    Add request and response models and prepare post method

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,37 @@
-use axum::Router;
-use axum::routing::get;
+use axum::{Json, Router};
+use axum::body::Body;
+use axum::http::StatusCode;
+use axum::response::{IntoResponse, Response};
+use axum::routing::{get, post};
+use serde::Deserialize;
+
+#[derive(Deserialize)]
+struct QueryRequest{
+    query: String
+}
+
+struct QueryResponse{
+    data: String
+}
+
+impl IntoResponse for QueryResponse{
+    fn into_response(self) -> Response {
+        axum::response::Response::builder()
+            .status(StatusCode::OK)
+            .header("Content-Type", "text/plain")
+            .body(Body::from(self.data))
+            .unwrap()
+    }
+}
 
 #[tokio::main]
 async fn main() {
     let app = Router::new()
-        .route("/", get(|| async { "Work in progress" }));
+        .route("/", post(query));
     let listener = tokio::net::TcpListener::bind("0.0.0.0:8080").await.unwrap();
     axum::serve(listener, app).await.unwrap();
+}
+
+async fn query(Json(payload): Json<QueryRequest>) -> QueryResponse{
+    QueryResponse{data:format!("[WIP] - query: {}",payload.query)}
 }
